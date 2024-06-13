c3e928fab1d0e239964de8eda016dafd10ebb6de
seesaw-monster <seesawmonster777@gmail.com>
2024-06-13 17:34:02 +0900

    .vim を .lua に書き換え

---
+++ lua/keymaps.lua
@@ -0,0 +1,6 @@
+-- smooth moving
+vim.api.nvim_set_keymap('n', 'gj', 'gj', { noremap = false, silent = true })
+vim.api.nvim_set_keymap('n', 'gk', 'gk', { noremap = false, silent = true })
+vim.api.nvim_set_keymap('n', 'gl', 'l', { noremap = false, silent = true })
+vim.api.nvim_set_keymap('n', 'gh', 'h', { noremap = false, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>g', '', { noremap = true, silent = true })
