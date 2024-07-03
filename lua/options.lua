8fc4151c8aec1c219b578316e4aeefc4dadd4055
seesaw-monster <seesawmonster777@gmail.com>
2024-07-04 03:58:42 +0900

    terminal モードの設定

--- lua/options.lua
+++ lua/options.lua
@@ -1,16 +1,16 @@
 -- paste mode in using clipboard
-if vim.o.term:find("xterm") then
-  vim.o.t_SI = vim.o.t_SI .. "\27[?2004h"
-  vim.o.t_EI = vim.o.t_EI .. "\27[?2004l"
-  vim.o.pastetoggle = "\27[201~"
+-- if vim.o.term:find("xterm") then
+--   vim.o.t_SI = vim.o.t_SI .. "\27[?2004h"
+--   vim.o.t_EI = vim.o.t_EI .. "\27[?2004l"
+--   vim.o.pastetoggle = "\27[201~"
 
-  function XTermPasteBegin(ret)
-    vim.o.paste = true
-    return ret
-  end
+--   function XTermPasteBegin(ret)
+--     vim.o.paste = true
+--     return ret
+--   end
 
-  vim.api.nvim_set_keymap('i', '<Esc>[200~', 'v:lua.XTermPasteBegin("")', { noremap = true, expr = true, silent = true })
-end
+--   vim.api.nvim_set_keymap('i', '<Esc>[200~', 'v:lua.XTermPasteBegin("")', { noremap = true, expr = true, silent = true })
+-- end
 
 -- clipboard on WSL
 if vim.fn.has('wsl') == 1 then
