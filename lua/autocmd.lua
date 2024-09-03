19510be63a045368b2b6ca2f1e8fb62f1c29c0db
seesaw-monster <L0Flies022@gmail.com>
2024-09-04 03:42:11 +0900

    コメント行を引き継がないように変更

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -5,3 +5,10 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.textwidth = 0
   end,
 })
+
+-- don't continue comment line
+vim.api.nvim_command('autocmd FileType * setlocal formatoptions-=c formatoptions-=o')
+
+vim.opt.formatoptions:remove('c')
+vim.opt.formatoptions:remove('r')
+vim.opt.formatoptions:remove('o')
