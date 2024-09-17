954b4473dbadd531ab85ff5b0bfb257c73c6a213
seesaw-monster <seesawmonster777@gmail.com>
2024-09-17 12:20:50 +0900

    fern の問題解決（Karabiner-Elements の問題だった)

--- lua/filer.lua
+++ lua/filer.lua
@@ -1,2 +1,2 @@
 -- Ctrl+nでファイルツリーを表示/非表示する
-vim.api.nvim_set_keymap('n', '<C-n>', ':Fern . -reveal=% -drawer -toggle -width=40<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-n>', ':Fern . -reveal=% -drawer -toggle -width=30<CR>', { noremap = true, silent = true })
