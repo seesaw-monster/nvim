6e94283d7c0504025fa125ed28f9bd266bdd70b8
seesaw-monster <L0Flies022@gmail.com>
2024-12-06 14:32:56 +0900

    insert mode 中に<C-u> で大文字変換

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,3 +4,15 @@ vim.api.nvim_set_keymap('n', 'gk', 'gk', { noremap = false, silent = true })
 vim.api.nvim_set_keymap('n', 'gl', 'l', { noremap = false, silent = true })
 vim.api.nvim_set_keymap('n', 'gh', 'h', { noremap = false, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>g', '', { noremap = true, silent = true })
+
+-- Uppercase conversion
+vim.keymap.set("i", "<C-u>",
+    function()
+        local line = vim.fn.getline(".")
+        local col = vim.fn.getpos(".")[3]
+        local substring = line:sub(1, col - 1)
+        local result = vim.fn.matchstr(substring, [[\v<(\k(<)@!)*$]])
+        return "<C-w>" .. result:upper()
+    end,
+    {expr = true}
+)
