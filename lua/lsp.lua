c1a07c94f3de162f680a9379409cb3f5424208aa
seesaw-monster <seesawmonster777@gmail.com>
2025-01-15 23:08:21 +0900

    cmp 修正

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -52,10 +52,10 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ['<C-p>'] = cmp.mapping.select_prev_item(),
     ["<C-n>"] = cmp.mapping.select_next_item(),
-    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    -- ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    -- ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
-    ["<C-e>"] = cmp.mapping.close(),
+    -- ["<C-e>"] = cmp.mapping.close(),
   }),
   sources = cmp.config.sources({
     { name = "nvim_lsp" },
