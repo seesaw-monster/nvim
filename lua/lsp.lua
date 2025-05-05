fde8301051a6f61e906c2403be86fd2a5582a3a3
seesaw-monster <seesawmonster777@gmail.com>
2025-05-05 22:28:02 +0900

    Remove unnecessary settings

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -40,7 +40,8 @@ require("mason-lspconfig").setup_handlers {
 capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 -- lsp の設定後に追加
-vim.opt.completeopt = "menu,menuone,noselect"
+vim.opt.completeopt = "menu,menuone"
+-- vim.opt.completeopt = "menu,menuone,noselect"
 
 local cmp = require"cmp"
 cmp.setup({
@@ -52,14 +53,11 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ['<C-p>'] = cmp.mapping.select_prev_item(),
     ["<C-n>"] = cmp.mapping.select_next_item(),
-    -- ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    -- ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    -- ["<C-e>"] = cmp.mapping.close(),
   }),
   sources = cmp.config.sources({
     { name = "nvim_lsp" },
     { name = "luasnip" },
     { name = "buffer" },
-  })
+  }),
+  preselect = cmp.PreselectMode.Item,
 })
