27060110c8fcf653ce8b6f678e05fda9aa2d771e
seesaw-monster <seesawmonster777@gmail.com>
2025-05-05 23:51:23 +0900

    Assign <C-l> to confirm completion candidates

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -53,6 +53,7 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ['<C-p>'] = cmp.mapping.select_prev_item(),
     ["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<C-l>"] = cmp.mapping.confirm({ select = true }),
   }),
   sources = cmp.config.sources({
     { name = "nvim_lsp" },
